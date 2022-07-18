f222af1e5b1df2fc74a4144155e5d7341db6b9b9
Moyowaaaa <moyowa7@icloud.com>
2022-07-18 09:05:50 +0100

    added more code

--- dist/index.js
+++ dist/index.js
@@ -75,3 +75,128 @@ function getArray(items) {
 }
 const numArray = getArray([1, 2, 3, 4]);
 const strArray = getArray(['brad', 'will', 'smith']);
+const eligiblity = 20;
+if (eligiblity >= 20) {
+    console.log("you can vote");
+}
+else {
+    console.log("you cant vote");
+}
+//tuple
+let Persoo = { name: "Moyowa", age: 20, agee: 20 };
+console.log(Persoo.name);
+//test
+const companies = [
+    { name: "Company One", category: "Finance", start: 1981, end: 2003 },
+    { name: "Company Two", category: "Marketing", start: 1981, end: 2007 },
+    { name: "Company Three", category: "Banking", start: 1921, end: 2015 },
+    { name: "Company Four", category: "Insurance", start: 1981, end: 2013 },
+    { name: "Company Five", category: "Finance", start: 1951, end: 2003 },
+    { name: "Company Six", category: "Auto", start: 1971, end: 2003 },
+    { name: "Company Seven", category: "Retail", start: 1991, end: 1992 },
+    { name: "Company Eight", category: "Finance", start: 1999, end: 2012 },
+    { name: "Company Nine", category: "Retail", start: 1980, end: 2002 },
+    { name: 'Company Ten', category: "Tech", start: 1999, end: 2012 },
+];
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+//for each
+// companies.forEach(function(company){
+//     console.log(company.name)
+// })
+//for Each with arrow function
+companies.forEach(company => {
+    console.log(company.name);
+});
+//filter
+const filteredcompany = companies.filter(function (company) {
+    return company.category === 'Finance';
+});
+console.log(filteredcompany);
+//sort ages
+const sorted = ages.sort(function (a, b) {
+    return a - b;
+});
+console.log(sorted);
+//sprt companies by start year
+const sortedcompanies = companies.sort(function (company1, company2) {
+    if (company1.start > company2.start) {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+});
+console.log(sortedcompanies);
+//reduce ages
+const agesum = ages.reduce(function (total, age) {
+    return total + age;
+});
+console.log(agesum);
+//map
+const companynames = companies.map(function (company) {
+    return company.name;
+});
+console.log(companynames.toString());
+//add x and y 
+const a = 10;
+let b = 20;
+function join(a, b) {
+    return a + b;
+}
+console.log(join(a, b));
+const talk = (message) => {
+    console.log(message);
+};
+talk('hello');
+const Precious1 = {
+    id: 1,
+    name: 'precious'
+};
+console.log(Precious1.name);
+const Moyowa = {
+    id: 1,
+    name: 'Moyowa',
+    languages: ['html', 'css', 'javascript']
+};
+console.log(Moyowa.languages, Moyowa.name);
+const check = (f, s) => f + s;
+console.log(check);
+class aNigga {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+const Peter = new aNigga(1, 'Peter');
+console.log(Peter.name);
+//class 
+class place {
+    constructor(id, name, region) {
+        this.id = id;
+        this.name = name;
+        this.region = region;
+    }
+    register() {
+        return `${this.name} is a company`;
+    }
+}
+const moyowaCompany = new place(1, 'M-technologies', 'Nigeria');
+const preciousCompany = new place(1, 'Ugochi', 'Lagos');
+console.log(moyowaCompany.register(), preciousCompany.register());
+//subclass
+class placeSubClass extends place {
+    constructor(id, name, region, revenue) {
+        super(id, name, region);
+        this.revenue = revenue;
+    }
+}
+const rev = new placeSubClass(4, 'Peter', 'Dubai', '3.3billion');
+console.log(rev.name, rev.register());
+//generics 
+function showArray(items) {
+    return new Array().concat(items);
+}
+let numberArray = showArray([1, 2, 3, 4, 5]);
+let stringArray = showArray(['Moyowa', 'Peter']);
+stringArray.push('1');
+console.log(stringArray);
--- src/index.ts
+++ src/index.ts
@@ -157,3 +157,237 @@ const numArray = getArray<number>([1,2,3,4])
 const strArray = getArray<string>(['brad','will','smith'])
 
 
+
+
+const eligiblity:number = 20 
+
+if (eligiblity >= 20){
+    console.log("you can vote")
+}
+else{
+    console.log("you cant vote")
+}
+
+
+//tuple
+
+let Persoo: {name:string,age:number, agee:number} = {name:"Moyowa", age:20, agee:20}
+
+
+console.log(Persoo.name )
+
+
+//test
+const companies:any[] = [
+    {name: "Company One", category: "Finance", start:1981, end:2003},
+    {name: "Company Two", category: "Marketing", start:1981, end:2007},
+    {name: "Company Three", category: "Banking", start:1921, end:2015},
+    {name: "Company Four", category: "Insurance", start:1981, end:2013},
+    {name: "Company Five", category: "Finance", start:1951, end:2003},
+    {name: "Company Six", category: "Auto", start:1971, end:2003},
+    {name: "Company Seven", category: "Retail", start:1991, end:1992},
+    {name: "Company Eight", category: "Finance", start:1999, end:2012},
+    {name: "Company Nine", category: "Retail", start:1980, end:2002},
+    {name: 'Company Ten', category: "Tech", start:1999, end:2012},
+   
+]
+
+const ages:number[] = [33,12,20,16,5,54,21,44,61,13,15,45,25,64,32];
+
+//for each
+
+// companies.forEach(function(company){
+//     console.log(company.name)
+// })
+
+//for Each with arrow function
+companies.forEach(company => {
+    console.log(company.name)
+}
+)
+
+
+//filter
+const filteredcompany:any[] = companies.filter(function(company){
+    return company.category === 'Finance'
+}
+)
+
+console.log(filteredcompany)
+
+//sort ages
+
+const sorted = ages.sort(function(a:number,b:number) {
+    return a - b
+})
+
+console.log(sorted)
+
+//sprt companies by start year
+const sortedcompanies = companies.sort(function(company1:any,company2:any){
+    if (company1.start > company2.start){
+        return 1
+    }
+    else{
+        return -1
+    }
+})
+
+console.log(sortedcompanies)
+
+//reduce ages
+
+const agesum = ages.reduce(function(total:number,age:number){
+    return total + age
+}
+)
+
+console.log(agesum)
+
+//map
+const companynames:any[] = companies.map(function(company){
+    return company.name
+}
+)
+
+console.log(companynames.toString())
+
+//add x and y 
+
+const a:number = 10;
+let b:number = 20
+
+function join (a:number, b:number):number {
+    return a + b
+}
+
+console.log(join(a,b))
+
+
+const talk = (message:string | number):void => {
+    console.log(message)
+}
+talk('hello')
+
+
+//intefaces 
+
+
+interface Precious {
+    id:number,
+    name:string
+}
+
+const Precious1: Precious = {
+    id:1,
+    name: 'precious'
+}
+
+console.log(Precious1.name)
+
+
+interface DeveloperInterface {
+    id:number,
+    name:string,
+    languages:any[]
+    age?:number
+}
+
+const Moyowa: DeveloperInterface = {
+    id:1,
+    name: 'Moyowa',
+    languages: ['html','css','javascript']
+
+}
+
+
+
+console.log(Moyowa.languages, Moyowa.name)
+
+interface blah {
+    (f:number, s:number):number
+}
+
+
+const check: blah = (f:number, s:number): number => f+s
+
+console.log(check)
+
+
+class aNigga {
+    id:number
+    name:string
+
+
+    constructor(id:number, name:string) {
+        this.id = id
+        this.name = name
+    }
+}
+
+const Peter = new aNigga(1, 'Peter')
+
+console.log(Peter.name)
+
+//interface
+interface CompanyInterface {
+    id?:number,
+    name?:string
+    region?:string,
+    register(): string
+}
+//class 
+class place implements CompanyInterface {
+    id?:number
+    name?:string
+    region?:string
+
+    constructor(id:number, name:string,region:string) {
+        this.id = id
+        this.name = name
+        this.region = region
+    }
+    register() {
+        return `${this.name} is a company`
+    }
+
+
+}
+
+const moyowaCompany = new place(1,'M-technologies','Nigeria')
+const preciousCompany = new place(1,'Ugochi','Lagos')
+
+
+console.log(moyowaCompany.register(), preciousCompany.register())
+
+//subclass
+class placeSubClass extends place {
+    revenue: string
+
+    constructor(id:number, name:string,region:string, revenue:string) {
+        super(id,name,region)
+        this.revenue = revenue
+    }
+
+
+
+}
+
+const rev = new placeSubClass(4,'Peter','Dubai','3.3billion')
+
+console.log(rev.name, rev.register())
+
+
+//generics 
+function showArray<T>(items:T[]): T[] {
+    return new Array().concat(items)
+}
+
+let numberArray = showArray<number>([1,2,3,4,5])
+let stringArray = showArray<string>(['Moyowa','Peter'])
+
+
+stringArray.push('1')
+console.log(stringArray)
+
+
